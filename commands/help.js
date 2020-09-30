4766c742789bf78766210fa34b0609eeaeac9bb8
Muhammad Iqbal Imani Atfan <42773954+Iotatfan@users.noreply.github.com>
2020-09-30 09:05:52 +0700

    testing help

--- commands/help.js
+++ commands/help.js
@@ -2,6 +2,7 @@ module.exports = {
     name: 'help',
     description: 'list of usefull command',
     execute(message, client) {
-        console.log(client)
+        // console.log(client)
+        message.channel.send(client)
     }    
 }
