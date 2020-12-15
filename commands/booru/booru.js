d84f2f187f4e62cc83ba69b700ad86c6ef4c0eb2
Muhammad Iqbal Imani Atfan <42773954+Iotatfan@users.noreply.github.com>
2020-12-15 17:52:08 +0700

    fix incorrect pixiv link

--- commands/booru/booru.js
+++ commands/booru/booru.js
@@ -1,4 +1,5 @@
 const Booru = require('booru')
+const pxBaseLink = 'https://www.pixiv.net/en/artworks/'
 
 module.exports = async (message, targetCh, tags, client, rating = 'safe') => {
   switch (rating) {
@@ -23,26 +24,32 @@ module.exports = async (message, targetCh, tags, client, rating = 'safe') => {
 
     if (searchResult.posts.length < 1) return message.reply('Please make sure you the tag is correct')
     else {
-      searchResult.forEach(post => {
-        const toSend = {
-          embed: {
-            image: {
-              url: post.fileUrl
-            },
-            fields: [
-              { name: 'Rating', value: post.rating, inline: true },
-              { name: 'Post', value: post.postView },
-              { name: 'rating : ', value: post.source != null ? post.source : 'rating di Post' }
-            ]
-          }
+      let source = null
+      if (searchResult[0].source.includes('pximg')) {
+        const str = searchResult[0].source.split('/').pop().toString().split('_')
+        source = pxBaseLink + str[0]
+      } else {
+        source = searchResult[0].source
+      }
+
+      const toSend = {
+        embed: {
+          image: {
+            url: searchResult[0].file_url
+          },
+          fields: [
+            { name: 'Rating', value: searchResult[0].rating, inline: true },
+            { name: 'Booru Post', value: searchResult[0].postView },
+            { name: 'Source : ', value: source != null ? source : 'Source di Post' }
+          ]
         }
-        if (targetCh && client) {
-          const ch = client.channels.cache.get(targetCh)
-          ch.send(toSend)
-        } else {
-          message.channel.send(toSend)
-        }
-      })
+      }
+      if (targetCh && client) {
+        const ch = client.channels.cache.get(targetCh)
+        ch.send(toSend)
+      } else {
+        message.channel.send(toSend)
+      }
     }
   } catch (err) {
     console.log(err)
